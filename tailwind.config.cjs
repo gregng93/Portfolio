3b9558bb032b245d61954636b826ad16e0e6190f
gregng93 <gregng93@gmail.com>
2025-10-09 04:01:40 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./index.html', './src/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        zoomIn: {
+          '0%': { transform: 'scale(0.8)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        },
+      },
+      animation: {
+        zoomIn: 'zoomIn 0.25s ease-out',
+      },
+    },
   },
   plugins: [],
 }
